7b901039ef93e2af62c8d8821060c7875f86ee4a
Ari Itkonen <aitkonen74@gmail.com>
2021-07-01 10:56:53 +0300

    python changed to use port from environment variable used by herkoku

--- src/main.py
+++ src/main.py
@@ -16,4 +16,8 @@ def bad_joke():
     return '<p>*** BAD JOKE ***</p>' + jokes[line] + '<p>****************</p>'
 
 if __name__ == "__main__":
-  app.run(host ='0.0.0.0')    
+  #app.run(host ='0.0.0.0')
+  # For flask running in Heroku
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
